56cc1976bd1483aec23249a6b695973f2fddafd6
satanic <cflikethepsp@gmail.com>
2020-01-06 22:48:44 +0900

    Delete example.test.cpp

--- example.test.cpp
+++
@@ -1,9 +0,0 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
-#include <iostream>
-
-int main() {
-    int a, b;
-    std::cin >> a >> b;
-    std::cout << a + b << std::endl;
-    return 0;
-}
